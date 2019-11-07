16c5e4b20aebdddbf580f8a16c55e3591d80b80d
ionicstuff <vaibhav.nadgonde1980@gmail.com>
2019-11-07 18:38:13 +0530

    api implemented

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
+import { AuthConstants } from '../config/auth-constants';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public postData = {
+    email: '',
+    password: ''
+  }
+
+  constructor(
+    private router: Router,
+    private authServices: AuthService,
+    private storageSevice: StorageService
+  ) { }
 
   ngOnInit() {
   }
 
+  validateInputs() {
+    let username = this.postData.email.trim();
+    let password = this.postData.password.trim();
+    return (
+      this.postData.email &&
+      this.postData.password &&
+      username.length > 0 &&
+      password.length > 0
+    );
+  }
+
+  loginAction() {
+    if (this.validateInputs()) {
+      this.authServices.login(this.postData).subscribe(
+        (res: any) => {
+          if (res.userData) {
+            // Storing the User data.
+            this.storageSevice.store(AuthConstants.AUTH, res.userData);
+            this.router.navigate(['home']);
+          } else {
+            console.log('incorrect password.');
+          }
+        },
+        (error: any) => {
+          console.log('Network Issue.');
+        }
+      );
+    } else {
+      console.log('Please enter email/username or password.');
+    }
+  }
+
 }
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:'https://api2.tribeathlon.co.uk/auth/'
 };
 
 /*
